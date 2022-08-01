35f9dd93b06ed58edccbcce33c761d02b5eda967
Mohamed Abdeen <mohamedabden21@gmail.com>
2022-08-01 17:19:32 +0200

    added start and end topics

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ RUNTIME = 600
 
 # Kafka
 SERVER_PORT = "localhost:9092"
-TOPIC = "quickstart-events"
-
+SOURCE_TOPIC = "quickstart-events"
+START_EVENTS_TOPIC = "start-events"
+END_EVENTS_TOPIC = "end-events"
