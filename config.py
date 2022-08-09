3bd34a47e971532845dd275a70a0279effec8fdc
Mohamed Abdeen <mohamedabden21@gmail.com>
2022-08-09 07:59:05 +0200

    updated config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ RUNTIME = 60 * 60
 SERVER_PORT = "localhost:9092"
 SOURCE_TOPIC = "quickstart-events"
 
-# Postgres JAR
-PSQLJAR = "/home/mohamed/US_Accidents_Analysis/Data/postgresql-42.4.1.jar"
-
 # Data directory
 DATADIR = "/home/mohamed/US_Accidents_Analysis/Data/"
+
+# Postgres JAR
+PSQLJAR = DATADIR + "postgresql-42.4.1.jar"
